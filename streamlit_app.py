0faa97eefa7f6fe7e7c8776455c5c1dd501fc04b
Arthemyst <pawel.lukmin@gmail.com>
2022-09-20 21:38:59 +0200

    Change: Modified results dataframe creation.

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,15 +84,23 @@ if estimation_button:
 
     df_new = pd.DataFrame(
         {
-            "overal_qual": [overal_qual],
-            "total_bsmt_sf": [total_bsmt_sf],
-            "floor_1st_sf": [floor_1st_sf],
-            "gr_liv_area": [gr_liv_area],
-            "garage_cars": [garage_cars],
-            "garage_area": [garage_area],
-            "sale_price": [predicted_price],
+            "Overal quality": [overal_qual],
+            "Total basement area": [total_bsmt_sf],
+            "First floor": [floor_1st_sf],
+            "Living area": [gr_liv_area],
+            "Garage cars": [garage_cars],
+            "Garage area": [garage_area],
+            "Sale price": [round(predicted_price, 2)],
         }
     )
     st.session_state.mdf = pd.concat([st.session_state.mdf, df_new], axis=0)
     st.write(f"Price: {predicted_price}$")
+if len(st.session_state.mdf) > 0:
+    st.session_state.mdf.reset_index(drop=True, inplace=True)
     st.dataframe(st.session_state.mdf)
+    st.download_button(
+        label="Download data as CSV",
+        data=st.session_state.mdf.to_csv().encode("utf-8"),
+        file_name="houses.csv",
+        mime="text/csv",
+    )
